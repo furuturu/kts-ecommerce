69f7b7e3ef155b5f2f88274f5e37b82b7b5c6897
furuturu <furufuru@yandex.ru>
2025-04-14 13:59:27 +0300

    refactor: move API request logic to mobix store

--- src/services/getProductsBySearch.ts
+++
@@ -1,9 +0,0 @@
-import { strapi } from "./axios.ts";
-import { searchProductQuery } from "./queries.ts";
-
-export const getProductsBySearch = async (searchQuery: string) => {
-  const response = await strapi.get("products", {
-    params: searchProductQuery(searchQuery),
-  });
-  return response.data;
-};
--- src/services/getProductsList.ts
+++
@@ -1,9 +0,0 @@
-import { strapi } from "./axios.ts";
-import { listProductsByPageQuery } from "./queries.ts";
-
-export const getProductsList = async (page: number) => {
-  const response = await strapi.get("products", {
-    params: listProductsByPageQuery(page),
-  });
-  return response.data;
-};
