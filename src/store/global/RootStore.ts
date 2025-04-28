7bb257f6efae180a577354956fb33aa398931b99
furuturu <furufuru@yandex.ru>
2025-04-28 10:05:46 +0300

    feat: add new stores

--- src/store/global/RootStore.ts
+++ src/store/global/RootStore.ts
@@ -1,9 +1,13 @@
 import { QueryParamsStore } from "./QueryParamsStore.ts";
 import { ApiStore } from "./ApiStore.ts";
+import { CartStore } from "./CartStore.ts";
+import { Navigation } from "./Navigation.ts";
 
 export class RootStore {
-  readonly query = new QueryParamsStore();
+  readonly query = new QueryParamsStore(this);
   readonly api = new ApiStore();
+  readonly cart = new CartStore();
+  readonly navigation = new Navigation();
 }
 
 export const rootStore = new RootStore();
